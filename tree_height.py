5386f3d91f759fcf1b865fedda8447b6de85cdb8
annijara <99959888+annijara@users.noreply.github.com>
2023-03-06 00:03:43 +0200

    updd

--- tree_height.py
+++ tree_height.py
@@ -1,69 +1,61 @@
-# python3
+ # python3
 
 import sys
 import threading
 import numpy
 
+def get_height(parents):
+     depths = [-1] * len(parents)
 
-def compute_height(parents):
-# Write this function
-    #max_height = 0
-# Your code here
-    #return max_height
-    n = len(parents)
-    length = [-1] * n
-    
-    def max_height(i):
-        if i == -1:
-            return -1
-         
-        if length[i] == -1:
-            length[i] = 1 + compute_height(parents[i])                      
-            return length[i]
-        
-        for i in range(n):
-            max_height(i)
-            return max(length)  
+     def get_depth(i):
+         if i == -1:
+             return -1
+         if depths[i] == -1:
+             depths[i] = 1 + get_depth(parents[i])
+         return depths[i]
+
+     for i in range(len(parents)):
+         get_depth(i)
+     return max(depths)
   
+#def compute_height(n, parents):
+    # Write this function
+    #max_height = 0
+    #return max_height
+    #pass
     
 
 def main():
-    option = input()
-    if "I" in option:
-        n = int(input())
-        text = input()
-        text = text.split()
-        parents = (list(map(int, text)))
-        
-        height = (compute_height( parents))
+    modee = input()
+    if "I" in modee:
+        int(input())
+        parents = (list(map(int, input().split())))
+        height = get_height(parents)
         print(height)
-    
-    elif "F" in option:
+        print(height+1)
+    elif "F" in modee:
         num = input()
-        if num != "a":
-            with open("./test/"+ num, mode="r") as fails:
-                text = fails.read()
-                x = text.splitlines()
-                #n = int(x[0])
-                txt = x[1].split()
-                parents = (list(map(int, txt)))
-                height = (compute_height(parents))
-                print(height)
+        with open("./test/"+ num, mode="r") as fails:
+            text = fails.read()
+            x = text.splitlines()
+            txt = x[1]
+            parents = (list(map(int, txt.split())))
+            height = get_height(parents)
+            print(height+1)
             
         # text = input("T: ")
         # text = text.split()
         # text = map(int, text)
         # text = list(text)
         # print (n, text) 
-        #list(map(input().split()))   
-            
+        #list(map(input().split()))           
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
     #pass
-   
+
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
