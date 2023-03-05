be934ffdcb6940b57fbe087d1b411f83b16a2404
annijara <99959888+annijara@users.noreply.github.com>
2023-03-05 22:44:23 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -4,47 +4,30 @@ import sys
 import threading
 import numpy
 
-class Node:
+def build_tree(root_node, nodes):
+    children = [
+        build_tree(child, nodes)
+        for child, node in enumerate(nodes)
+        if node == root_node
+    ]
+    return {'key': root_node, 'children': children}
 
-	# Constructor to create a new node
-	def __init__(self, data):
-		self.data = data
-		self.left = None
-		self.right = None
+def compute_height(tree):
+    return 1 + max((compute_height(c) for c in tree['children']), default=-1)
   
-  
-  
-def compute_height(n, parents):
+#def compute_height(n, parents):
     # Write this function
-    max_height = 0
-    if parents is None:
-        return 0
-    else:
-
-		# Compute the depth of each subtree
-            lDepth = compute_height(parents.left)
-            rDepth = compute_height(parents.right)
-            
-            if (lDepth > rDepth):
-             return lDepth+1
-            else:
-             return rDepth+1
-    # Your code here
+    #max_height = 0
     #return max_height
+    #pass
     
 
 def main():
     modee = input("mode: ")
     if "I" in modee:
-        n = int(input("Num: "))
-        text = input("T: ")
-        text = text.split()
-        text = map(int, text)
-        text = list(text)
-        print (n, text)
-        #list(map(input().split()))
-        compute_height(n, text)
-        
+        int(input())
+        tree = build_tree(-1, list(map(int, input().split())))
+        print(compute_height(tree))
     elif "F" in modee:
         num = input("dok nr: ")
         with open("./test/"+ num, mode="r") as fails:
@@ -54,9 +37,15 @@ def main():
             n = x[0]
             parent = x[1]
             compute_height(n, parent)
+            
+        # text = input("T: ")
+        # text = text.split()
+        # text = map(int, text)
+        # text = list(text)
+        # print (n, text) 
+        #list(map(input().split()))           
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
-    print("Height of tree is %d" % (compute_height(text)))
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
