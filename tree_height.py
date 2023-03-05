772d7ee4db023f433f0911895f79b34dfa5e7a62
annijara <99959888+annijara@users.noreply.github.com>
2023-03-06 00:10:47 +0200

    new update

--- tree_height.py
+++ tree_height.py
@@ -4,19 +4,19 @@ import sys
 import threading
 import numpy
 
-def get_height(parents):
-     depths = [-1] * len(parents)
+def compute_height(n, parents):
+    length = [-1] * n
+    
+    def max_height(i):
+        if i == -1:
+            return -1
+        if length[i] == -1:
+            length[i] = 1 + max_height(parents[i])
+        return length[i]
 
-     def get_depth(i):
-         if i == -1:
-             return -1
-         if depths[i] == -1:
-             depths[i] = 1 + get_depth(parents[i])
-         return depths[i]
-
-     for i in range(len(parents)):
-         get_depth(i)
-     return max(depths)
+    for i in range(n):
+        max_height(i)
+    return max(length)
   
 #def compute_height(n, parents):
     # Write this function
@@ -28,27 +28,30 @@ def get_height(parents):
 def main():
     modee = input()
     if "I" in modee:
-        int(input())
-        parents = (list(map(int, input().split())))
-        height = get_height(parents)
-        print(height)
-        print(height+1)
+        n = int(input())
+        text = input()
+        text = text.split()
+        parents = (list(map(int, text)))
+        height = compute_height(n, parents)
+        print(height + 1)
     elif "F" in modee:
         num = input()
         with open("./test/"+ num, mode="r") as fails:
             text = fails.read()
             x = text.splitlines()
+            n = int(x[0])
             txt = x[1]
             parents = (list(map(int, txt.split())))
-            height = get_height(parents)
-            print(height+1)
+            height = compute_height(n, parents)
+            print(height + 1)
             
         # text = input("T: ")
         # text = text.split()
         # text = map(int, text)
         # text = list(text)
         # print (n, text) 
-        #list(map(input().split()))           
+        #list(map(input().split())) 
+                  
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
     # input number of elements
@@ -63,4 +66,5 @@ sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 #threading.Thread(target=main).start()
 main()
+
 # print(numpy.array([1,2,3]))
